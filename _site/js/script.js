2d6066ad350747b9dffbe5f5f4a2bb22e9a9e585
Linda Zhou <lz28330@gmail.com>
2018-07-30 21:02:15 -0400

    getting some animation shown

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,2 +1,11 @@
 //TweenMax.to(target, duration, {vars: ex. delays, callbacks,eases})
-TweenMax.to(".logo", 2, {left:600});
+
+//wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+  // wait until window is loaded - all images, links, css, scripts, fonts, and other media assets
+  window.addEventListener("load", function(){
+       // run animation code here
+       TweenMax.to(".logo", 2, {x:600});
+
+  });
+});
