b892b0086e42da0e1f890d2d528f9df9664b82bd
Linda Zhou <lz28330@gmail.com>
2018-08-01 02:56:45 -0400

    smoother transition

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
        // TweenMax.to(".logo", 2, {x:600});
        // TweenMax.to(".logo", 2, {x:600, onComplete:complete});
        var gradient = TweenMax.to(".bg", 5, {backgroundColor:"#B76E79"});
-       var flower = TweenMax.to(".crown", 3, {x:500, y:-300, opacity: 0.7, delay: 0.3});
+       var flower = TweenMax.to(".crown", 2, {x:500, y:-300, opacity: 0.3, delay: 0.3});
        var doge = TweenMax.to(".logo", 2, {x:600, opacity:0.5, scale: 2});
        var title= TweenMax.from(".title", 2, {x:500, opacity:0, scale:0.5, delay: 0.5});
 
